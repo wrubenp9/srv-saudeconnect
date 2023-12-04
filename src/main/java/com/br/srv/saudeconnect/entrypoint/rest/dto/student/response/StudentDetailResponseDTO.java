08abdb3d049b71f0713db60c724ff43b2fdc2b19
wruben <wesleyruben213@hotmail.com>
2023-12-04 16:35:02 -0300

    ajustar studentResponseDTO

--- src/main/java/com/br/srv/saudeconnect/entrypoint/rest/dto/student/response/StudentDetailResponseDTO.java
+++ src/main/java/com/br/srv/saudeconnect/entrypoint/rest/dto/student/response/StudentDetailResponseDTO.java
@@ -26,5 +26,5 @@ public class StudentDetailResponseDTO {
     private AddressResponseDTO address;
     private ContactResponseDTO contact;
     private AccountResponseDTO account;
-    private List<GoalResponseDTO> Goals;
+    private List<GoalResponseDTO> goals;
 }
